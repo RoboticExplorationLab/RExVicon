b23306d9550bda6cc7332509d131cbc036449195
Brian Jackson <bjack205@gmail.com>
2021-09-21 17:12:29 -0400

    Convert from fixed-point back to floats in teensy

--- src/arduino/teensy_receiver/teensy_receiver.ino
+++ src/arduino/teensy_receiver/teensy_receiver.ino
@@ -1,7 +1,10 @@
 #include "receiver.hpp"
 #include "pose.hpp"
 
+#include "limits"
+
 using ViconMsg = rexlab::Pose<int32_t>;
+using Pose = rexlab::Pose<float>;
 
 // Global Parameters
 float rate_print_frequency = 1.0;  // frequency at which to print the averaged rate
@@ -10,6 +13,7 @@ constexpr HardwareSerial& HolybroRadio = Serial5;
 // Global Variables
 constexpr int BUFLEN = sizeof(ViconMsg);
 ViconMsg msg;
+Pose pose;
 char* buf = reinterpret_cast<char*>(&msg);
 int x_prev = -1;
 rexlab::SerialReceiver receiver(HolybroRadio, ViconMsg::MsgID());
@@ -25,6 +29,16 @@ void setup() {
 }
 
 // the loop function runs over and over again forever
+int count = 0;
 void loop() {
   bool msg_received = receiver.Receive(buf, BUFLEN);
+  pose = rexlab::ConvertPoseIntToFloat<float>(msg);
+  int Tmax = std::numeric_limits<int32_t>::max();
+//  Serial.print((float)msg.position_z / (float)Tmax);
+  if (count == 10) {
+    Serial.print(pose.position_z);
+    Serial.print("\n");
+    count = 0;
+  }
+  ++count;
 }
